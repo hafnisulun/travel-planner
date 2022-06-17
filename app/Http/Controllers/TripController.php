61a8b115f1d0f67708dd11aec6a8731b921fbb7b
Hafni Sulun <hafni.sulun@gmail.com>
2022-06-17 22:55:01 +0700

    Remove unnecessary use in trip controller

--- app/Http/Controllers/TripController.php
+++ app/Http/Controllers/TripController.php
@@ -9,7 +9,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
-use Symfony\Component\Console\Input\Input;
 
 class TripController extends Controller
 {
